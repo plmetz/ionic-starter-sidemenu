7a7496dfb2f8f5b9e8031194ade5769df29b8866
Paul Metzler <plmetz@gmail.com>
2018-05-24 22:08:47 -0400

    circle ci workflow test

--- .circleci/run-build-locally.sh
+++ .circleci/run-build-locally.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 curl --user 9188d881d648eba94dbdd1c7ce06c6cb5b408cba: \
     --request POST \
-    --form revision=703928fffe3f099abe47562ea7053f82ca470d66 \
+    --form revision=5555c45e0fe1d2940775a285ec310c999599224e \
     --form config=@config.yml \
     --form notify=false \
-        https://circleci.com/api/v1.1/project/github/plmetz/ionic-starter-sidemenu/tree/features/e2e_test
+        https://circleci.com/api/v1.1/project/github/plmetz/ionic-starter-sidemenu/tree/features/ci
